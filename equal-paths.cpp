73ef4851a66a9af13c74b1b5f3d03cb970d5ccfe
Vaneet Saini <vsaini@usc.edu>
2022-10-28 09:02:38 +0000

    equal paths commit

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,13 +1,22 @@
 #include "equal-paths.h"
+#include <iostream> 
 using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
-
+int height(Node* root)
+{
+    if (root == nullptr) return 0;
+    return 1 + max(height(root->left), height(root->right));
+}
 bool equalPaths(Node * root)
 {
-    // Add your code below
-
+    int r, l;
+    if (root == nullptr) return true;
+    l = height(root->left);
+    r= height(root->right);
+    if ((l == 0 && r == 1) || (r == 0 && l == 1)) return true;
+    if (l == r) return true;
+    return false;
 }
 
